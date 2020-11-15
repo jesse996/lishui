3cd138490d4cc9d9b67e83d3e08b4ab20bf21eee
jesse <szx961102@gmail.com>
2020-11-15 15:54:59 +0800

    init

---
+++ src/main/java/com/example/lishui/LishuiApplication.java
@@ -0,0 +1,13 @@
+package com.example.lishui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LishuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LishuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lishui/LishuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lishui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LishuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
