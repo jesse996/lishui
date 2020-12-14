714f6e06186d65294cb639d16ac5e548df5b1d23
jesse <szx961102@gmail.com>
2020-12-14 16:27:13 +0800

    add login document and verify code controller

---
+++ src/main/java/com/example/lishui/controller/VerifyCodeController.java
@@ -0,0 +1,27 @@
+package com.example.lishui.controller;
+
+import com.example.lishui.common.utils.VerifyCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Created by jesse on 2020/12/14 下午4:16
+ */
+@RestController
+public class VerifyCodeController {
+    @GetMapping("/api/vercode")
+    public void code(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        VerifyCode vc = new VerifyCode();
+        BufferedImage image = vc.getImage();
+        String text = vc.getText();
+        HttpSession session = req.getSession();
+        session.setAttribute("index_code", text);
+        VerifyCode.output(image, resp.getOutputStream());
+    }
+}
