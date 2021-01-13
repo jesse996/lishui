f39c6b53e86d2607cbca4b36ae882aa5dc884202
Jesse <szx961102@gmail.com>
2021-01-13 15:06:15 +0800

    去掉用户最后登录时间

--- src/main/java/com/example/lishui/dao/entity/User.java
+++ src/main/java/com/example/lishui/dao/entity/User.java
@@ -60,8 +60,8 @@ public class User implements Serializable {
     @Column(nullable = false)
     private Boolean status = true;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastLoginDateTime;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastLoginDateTime;
 //    @OneToMany(mappedBy = "user",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
 //    //级联保存、更新、删除、刷新;延迟加载。当删除用户，会级联删除该用户的所有文章
 //    //拥有mappedBy注解的实体类为关系被维护端
