abcd88283b05087eaf36f4bf5fa090c844a26899
Jesse <szx961102@gmail.com>
2021-01-22 12:24:21 +0800

    去掉path log

--- src/main/java/com/example/lishui/config/UploadConfig.java
+++ src/main/java/com/example/lishui/config/UploadConfig.java
@@ -1,26 +1,12 @@
 package com.example.lishui.config;
 
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-import org.springframework.boot.autoconfigure.web.WebProperties;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.boot.system.ApplicationHome;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ClassUtils;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.io.File;
-import java.net.URLDecoder;
 import java.nio.file.Paths;
 
 /**
@@ -33,9 +19,6 @@ public class UploadConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String dirPath= new ApplicationHome(this.getClass()).getDir().getAbsolutePath();
         dirPath = Paths.get(dirPath).resolve("upload").toString()+File.separator;
-        System.out.println("dirPath");
-        System.out.println(dirPath);
-
         //第三种
 //        String path1 = ClassUtils.getDefaultClassLoader().getResource("").getPath();
 //        System.out.println(URLDecoder.decode(path1, "utf-8"));
